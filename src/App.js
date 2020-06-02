ce26963d80e50a3b9063b3f2ada4e83ff0ad752e
lazychal <disposables@yandex.ru>
2020-06-03 00:36:59 +0300

    Styles. Body #1 - grid areas a-b-c

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './App.css';
+import s from './App.module.scss';
 import NavBar from "./components/Navbar/Navbar";
 import {Redirect, Route, Switch, withRouter} from "react-router-dom";
 import News from "./components/News/News";
@@ -22,12 +22,14 @@ class App extends React.Component {
         alert('Something is Bad...');
         // console.error(promise)
     };
+
     componentDidMount() {
         this.props.initializeApp();
 
         //Отлавливаем промисы reject(ошибки)
         window.addEventListener('unhandledrejection', this.catchAllUnhandledErrors)
     }
+
     componentWillUnmount() {
         // Подчищаем "мусор" за addEventListener
         window.removeEventListener('unhandledrejection', this.catchAllUnhandledErrors)
@@ -41,28 +43,46 @@ class App extends React.Component {
 
         return (
 
-            <div className="app-wrapper">
-                <HeaderContainer/>
-                <NavBar/>
-                <div className='app-wrapper-content'>
-                    <Switch>
-                        <Route exact path='/' render={ () => <Redirect to={'/profile'} /> }/>
-                        <Route path='/dialogs' render={ withSuspense(DialogsContainer) }/>
-                        {/*В строке ниже мы присваеваем URL доп. параметр, который будет считывать ID юзера.
+            <div className={s.appWrapper}>
+
+                <div className={s.appHeader}>
+                    <HeaderContainer/>
+                </div>
+
+                <div className={s.appBody}>
+                    <div className={s.areaA}>
+                        <NavBar/>
+                    </div>
+                    <div className={s.areaB}>
+                        <Switch>
+                            <Route exact path='/' render={() => <Redirect to={'/profile'}/>}/>
+                            <Route path='/dialogs' render={withSuspense(DialogsContainer)}/>
+                            {/*В строке ниже мы присваеваем URL доп. параметр, который будет считывать ID юзера.
                     Далее, в контейнерной компоненте, мы зоздаём переменную, с таким же названием(необязательно),
                     и присваиваем ей значение, приходящее в пропсы(смотрим в консоли): props.match.params.userId
                     И, наконец, в componentDidMount, в ajax-запросе плюсуем к URL нашу переменную
                     userId(или как мы там её назвали). Теперь страница будет отслеживать ID, и отрисовывать профили
                     любых, нужных нам пользователей.*/}
-                        <Route path='/profile/:userId?' render={ withSuspense(ProfileContainer) }/>
-                        <Route path='/users' render={() => <UsersContainer/>}/>
-                        <Route path='/news' render={() => <News/>}/>
-                        <Route path='/music' render={() => <Music/>}/>
-                        <Route path='/settings' render={() => <Settings/>}/>
-                        <Route path='/login' render={() => <Login/>}/>
-                        <Route path='*' render={() => <div>404 Not Found</div>}/>
-                    </Switch>
+                            <Route path='/profile/:userId?' render={withSuspense(ProfileContainer)}/>
+                            <Route path='/users' render={() => <UsersContainer/>}/>
+                            <Route path='/news' render={() => <News/>}/>
+                            <Route path='/music' render={() => <Music/>}/>
+                            <Route path='/settings' render={() => <Settings/>}/>
+                            <Route path='/login' render={() => <Login/>}/>
+                            <Route path='*' render={() => <div>404 Not Found</div>}/>
+                        </Switch>
+                    </div>
+
+                    <div className={s.areaC}>
+
+                    </div>
+
                 </div>
+
+                <div className={s.appFooter}>
+
+                </div>
+
             </div>
 
         );
--- src/components/Header/ProfileDropdown/ProfileDropdown.jsx
+++ src/components/Header/ProfileDropdown/ProfileDropdown.jsx
@@ -4,7 +4,7 @@ import {NavLink} from "react-router-dom";
 
 const ProfileDropdown = (props) => {
     return <div className={s.profileDropdown}>
-        
+
     </div>
 };
 
