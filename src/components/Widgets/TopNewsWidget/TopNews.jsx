33192ae6ab1ada6f335ff6f978c08e4170e26110
lazychal <disposables@yandex.ru>
2020-06-17 21:46:11 +0300

    CardInterestingPages - local state with hooks #2

--- src/components/Widgets/TopNewsWidget/TopNews.jsx
+++ src/components/Widgets/TopNewsWidget/TopNews.jsx
@@ -6,65 +6,65 @@ import news3 from "../../../assets/images/TopNewsThumbs/news-3.jpg";
 import news4 from "../../../assets/images/TopNewsThumbs/news-4.jpg";
 import news5 from "../../../assets/images/TopNewsThumbs/news-5.jpg";
 
-const TopNews = (props) => {
+const TopNews = ({state}) => {
     debugger
     return <div className={s.container}>
-        <h4 className={s.widgetTitle}>{props.state.pageTitle}</h4>
+        <h4 className={s.widgetTitle}>{state.pageTitle}</h4>
         <div className={s.widgetBody}>
             <ul className={s.newsPageListWrapper}>
                 <li className={s.listItem}>
                     <div className={s.listItemThumb}>
                         <a href="#">
-                            <img src={news1} alt="profile picture 35*35"/>
+                            <img src={state.news[0].img} alt="profile picture 35*35"/>
                         </a>
                     </div>
                     <div className={s.listItemInfo}>
-                        <h3 className={s.itemTitle}><a href="#">Hot News About Some</a></h3>
-                        <p className={s.itemSubtitle}><a href="#">4 Min Ago</a></p>
+                        <h3 className={s.itemTitle}><a href="#">{state.news[0].itemTitle}</a></h3>
+                        <p className={s.itemSubtitle}><a href="#">{state.news[0].publicationTime} Min Ago</a></p>
                     </div>
                 </li>
                 <li className={s.listItem}>
                     <div className={s.listItemThumb}>
                         <a href="#">
-                            <img src={news2} alt="profile picture 35*35"/>
+                            <img src={state.news[1].img} alt="profile picture 35*35"/>
                         </a>
                     </div>
                     <div className={s.listItemInfo}>
-                        <h3 className={s.itemTitle}><a href="#">Hot News About Some</a></h3>
-                        <p className={s.itemSubtitle}><a href="#">15 Min Ago</a></p>
+                        <h3 className={s.itemTitle}><a href="#">{state.news[1].itemTitle}</a></h3>
+                        <p className={s.itemSubtitle}><a href="#">{state.news[1].publicationTime} Min Ago</a></p>
                     </div>
                 </li>
                 <li className={s.listItem}>
                     <div className={s.listItemThumb}>
                         <a href="#">
-                            <img src={news3} alt="profile picture 35*35"/>
+                            <img src={state.news[2].img} alt="profile picture 35*35"/>
                         </a>
                     </div>
                     <div className={s.listItemInfo}>
-                        <h3 className={s.itemTitle}><a href="#">Hot News About Some</a></h3>
-                        <p className={s.itemSubtitle}><a href="#">21 Min Ago</a></p>
+                        <h3 className={s.itemTitle}><a href="#">{state.news[2].itemTitle}</a></h3>
+                        <p className={s.itemSubtitle}><a href="#">{state.news[2].publicationTime} Min Ago</a></p>
                     </div>
                 </li>
                 <li className={s.listItem}>
                     <div className={s.listItemThumb}>
                         <a href="#">
-                            <img src={news4} alt="profile picture 35*35"/>
+                            <img src={state.news[3].img} alt="profile picture 35*35"/>
                         </a>
                     </div>
                     <div className={s.listItemInfo}>
-                        <h3 className={s.itemTitle}><a href="#">Hot News About Some</a></h3>
-                        <p className={s.itemSubtitle}><a href="#">39 Min Ago</a></p>
+                        <h3 className={s.itemTitle}><a href="#">{state.news[3].itemTitle}</a></h3>
+                        <p className={s.itemSubtitle}><a href="#">{state.news[3].publicationTime} Min Ago</a></p>
                     </div>
                 </li>
                 <li className={s.listItem}>
                     <div className={s.listItemThumb}>
                         <a href="#">
-                            <img src={news5} alt="profile picture 35*35"/>
+                            <img src={state.news[4].img} alt="profile picture 35*35"/>
                         </a>
                     </div>
                     <div className={s.listItemInfo}>
-                        <h3 className={s.itemTitle}><a href="#">Hot News About Some</a></h3>
-                        <p className={s.itemSubtitle}><a href="#">47 Min Ago</a></p>
+                        <h3 className={s.itemTitle}><a href="#">{state.news[4].itemTitle}</a></h3>
+                        <p className={s.itemSubtitle}><a href="#">{state.news[4].publicationTime} Min Ago</a></p>
                     </div>
                 </li>
             </ul>
@@ -75,13 +75,13 @@ const TopNews = (props) => {
 export const TopNewsContainer = (props) => {
     const [state, setState] = useState({
         pageTitle: 'Latest Top News',
-        news: {
-            1: {img: news1, itemTitle: 'Hot News About Some', publicationTime: '4'},
-            2: {img: news2, itemTitle: 'Hot News About Some', publicationTime: '15'},
-            3: {img: news3, itemTitle: 'Hot News About Some', publicationTime: '21'},
-            4: {img: news4, itemTitle: 'Hot News About Some', publicationTime: '39'},
-            5: {img: news5, itemTitle: 'Hot News About Some', publicationTime: '47'},
-        },
+        news: [
+            {img: news1, itemTitle: 'Hot News About Some', publicationTime: '4'},
+            {img: news2, itemTitle: 'Hot News About Some', publicationTime: '15'},
+            {img: news3, itemTitle: 'Hot News About Some', publicationTime: '21'},
+            {img: news4, itemTitle: 'Hot News About Some', publicationTime: '39'},
+            {img: news5, itemTitle: 'Hot News About Some', publicationTime: '47'},
+        ],
 
     } );
     debugger
