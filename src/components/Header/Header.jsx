88b06105d4cc21b506daa7a59ed68cfffd628473
lazychal <disposables@yandex.ru>
2020-06-30 16:02:16 +0300

    Header.jsx - profileDropdown - #3 (login-logout+styles)

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,64 +7,105 @@ import {faFile} from "@fortawesome/free-regular-svg-icons/faFile";
 import {faCog, faUnlockAlt} from "@fortawesome/free-solid-svg-icons";
 
 const Header = (props) => {
-    debugger
+
     return <header className={s.header}>
         <div className={s.container}>
 
-                <div className={s.mainMenu}>
-                    <nav>
-                        <ul className={s.navUl}>
-                            <li><NavLink to='/' className={s.navLink}>Home</NavLink></li>
-                            <li><NavLink to='' className={s.navLink}>Message</NavLink></li>
-                            <li><NavLink to='' className={s.navLink}>Notification</NavLink></li>
-                        </ul>
-                    </nav>
+            <div className={s.mainMenu}>
+                <nav>
+                    <ul className={s.navUl}>
+                        <li><NavLink to='/' className={s.navLink}>Home</NavLink></li>
+                        <li><NavLink to='' className={s.navLink}>Message</NavLink></li>
+                        <li><NavLink to='' className={s.navLink}>Notification</NavLink></li>
+                    </ul>
+                </nav>
+            </div>
+            <div className={s.logo}>
+                <img alt='logo'
+                     src='https://www.watersignal.com/wp-content/uploads/2016/05/footstep_icon2-150x150.png'/>
+            </div>
+            <div className={s.loginBlock}>
+                <div className={s.searchContainer}>
+                    <form className={s.topSearchBox} action="">
+                        <input type="text" placeholder='Search' className={s.topSearchField}/>
+                        <button className={s.topSearchBtn}>S</button>
+                    </form>
                 </div>
-                <div className={s.logo}>
-                    <img alt='logo' src='https://www.watersignal.com/wp-content/uploads/2016/05/footstep_icon2-150x150.png'/>
-                </div>
-                <div className={s.loginBlock}>
-                    <div className={s.searchContainer}>
-                        <form className={s.topSearchBox} action="">
-                            <input type="text" placeholder='Search' className={s.topSearchField}/>
-                            <button className={s.topSearchBtn}>S</button>
-                        </form>
-                    </div>
-                    <div className={s.profileSettingBox}>
-                        {/*<a className={s.profileTrigger} href="" >*/}
-                            <img className={s.userPhoto} src={props.userPic} alt="UserData Mini"/>
-                        {/*</a>*/}
-                            <div className={s.profileDropdown}>
-                                <div className={s.profileHead}>
-                                    <h5 className={s.fullName}>
-                                        <NavLink to={'/'}>
-                                            {props.fullName}
-                                        </NavLink>
-                                    </h5>
+                <div className={s.profileSettingBox}>
+                    {/*<a className={s.profileTrigger} href="" >*/}
+                    <img className={s.userPhoto} src={props.userPic} alt="UserData Mini"/>
+                    {/*</a>*/}
+                    <div className={s.profileDropdown}>
+                        <div className={s.profileHead}>
+                            <ul>
+                                <li>
+                                    {/*<h5 className={s.fullName}>*/}
+                                    <NavLink to={'/'}>
+                                        {props.fullName}
+                                    </NavLink>
+                                    {/*</h5>*/}
+                                </li>
+                                <li>
                                     <NavLink to={'/'} className={s.email}>email@email.com</NavLink>
-                                </div>
-                                <div className={s.profileBody}>
-                                    <ul>
-                                        <li>
-                                            {/*Вариант, где ховер загорается отдельно или на тексте или на иконке...*/}
-                                            <a href="#">
-                                                <FontAwesomeIcon icon={faUser} className={s.listAwesomeIcons}> </FontAwesomeIcon>  Profile
-                                            </a>
-                                        </li>
-                                        <li><FontAwesomeIcon icon={faEnvelope} className={s.listAwesomeIcons}> </FontAwesomeIcon> Inbox</li>
-                                        <li><FontAwesomeIcon icon={faFile} className={s.listAwesomeIcons}> </FontAwesomeIcon> Activity</li>
-                                    </ul>
-                                    <ul>
-                                        <li><FontAwesomeIcon icon={faCog} className={s.listAwesomeIcons}> </FontAwesomeIcon> Settings</li>
-                                        <li><FontAwesomeIcon icon={faUnlockAlt} className={s.listAwesomeIcons}> </FontAwesomeIcon> Sign Out</li>
-                                    </ul>
-                                </div>
-                            </div>
+                                </li>
+                            </ul>
 
-                        {props.isAuth ? <div>{props.login} - <button onClick={props.logout}>Logout</button></div>
-                            : <NavLink to={'/login'}>Login</NavLink>}
+
+                        </div>
+                        <div className={s.profileBody}>
+                            <ul>
+                                <li>
+                                    <NavLink to={'/'}>
+                                        <FontAwesomeIcon icon={faUser}
+                                                         className={s.listAwesomeIcons}> </FontAwesomeIcon> Profile
+                                    </NavLink>
+                                </li>
+                                <li>
+                                    <NavLink to={'/'}>
+                                        <FontAwesomeIcon icon={faEnvelope}
+                                                         className={s.listAwesomeIcons}> </FontAwesomeIcon> Inbox
+                                    </NavLink>
+                                </li>
+                                <li>
+                                    <NavLink to={'/'}>
+                                        <FontAwesomeIcon icon={faFile}
+                                                         className={s.listAwesomeIcons}> </FontAwesomeIcon> Activity
+                                    </NavLink>
+                                </li>
+                            </ul>
+                            <ul>
+                                <li>
+                                    <NavLink to={'/'}>
+                                        <FontAwesomeIcon icon={faCog}
+                                                         className={s.listAwesomeIcons}> </FontAwesomeIcon> Settings
+                                    </NavLink>
+                                </li>
+                                {props.isAuth ?
+                                    <li onClick={props.logout} style={{cursor: 'pointer'}}>
+                                        <FontAwesomeIcon icon={faUnlockAlt}
+                                                         className={s.listAwesomeIcons}>
+                                        </FontAwesomeIcon> Sign Out
+                                    </li>
+                                    : <li>
+                                        <NavLink to={'/login'}>
+                                            <FontAwesomeIcon icon={faUnlockAlt}
+                                                             className={s.listAwesomeIcons}>
+                                            </FontAwesomeIcon>Sign In
+                                        </NavLink>
+                                    </li>
+                                }
+                            </ul>
+                        </div>
                     </div>
+
+                    {/*{props.isAuth ? <div>{props.login} - <button onClick={props.logout}>Logout</button></div>*/}
+                    {/*    : <NavLink to={'/login'}>Login</NavLink>}*/}
+
+                    {props.isAuth ?
+                        <li onClick={props.logout}>{props.login} Logout</li>
+                        : <li><NavLink to={'/login'}>Login</NavLink></li>}
                 </div>
+            </div>
 
         </div>
     </header>
