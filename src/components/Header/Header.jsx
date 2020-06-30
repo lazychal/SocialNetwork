fe4ee60146906a2f15fcd2c582541e6778c02a3a
lazychal <disposables@yandex.ru>
2020-06-30 21:35:08 +0300

    Header.jsx - refactor profileDropdownComponent

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,9 @@
-import React, {useEffect, useState} from 'react';
+import React, {useState} from 'react';
 import s from './Header.module.scss'
 import {NavLink} from "react-router-dom";
-import {faEnvelope, faUser} from "@fortawesome/free-regular-svg-icons";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faFile} from "@fortawesome/free-regular-svg-icons/faFile";
-import {faCog, faSearch, faUnlockAlt} from "@fortawesome/free-solid-svg-icons";
+import {faSearch} from "@fortawesome/free-solid-svg-icons";
+import ProfileDropdown from "./ProfileDropdown/ProfileDropdown";
 
 const Header = (props) => {
 
@@ -26,91 +25,30 @@ const Header = (props) => {
                 <img alt='logo'
                      src='https://www.watersignal.com/wp-content/uploads/2016/05/footstep_icon2-150x150.png'/>
             </div>
+
             <div className={s.loginBlock}>
                 <div className={s.searchContainer}>
                     <form className={s.topSearchBox} action="">
                         <input type="text" placeholder='Search' className={s.topSearchField}/>
-                        {/*<button className={s.topSearchBtn}>S</button>*/}
+
                         <FontAwesomeIcon icon={faSearch} style={{fontSize: '20px'}}> </FontAwesomeIcon>
                     </form>
                 </div>
                 <div className={s.profileSettingBox}>
-                    {/*<a className={s.profileTrigger} href="" >*/}
+
                     <img className={s.userPhoto} src={props.userPic} alt="UserData Mini"
-                         onClick={()=>{setDropWindow(!dropWindow)}}
+                         onClick={() => {
+                             setDropWindow(!dropWindow)
+                         }}
                     />
-                    {/*</a>*/}
-
-                    <div className={dropWindow ? s.profileDropdown : s.hideDropdown} onBlur={()=>{setDropWindow(!dropWindow)}}>
-                        <div className={s.profileHead}>
-                            <ul>
-                                <li>
-                                    <h5 className={s.fullName}>
-                                    <NavLink to={'/'}>
-                                        {props.fullName}
-                                    </NavLink>
-                                    </h5>
-                                </li>
-                                <li>
-                                    <NavLink to={'/'} className={s.email}>email@email.com</NavLink>
-                                </li>
-                            </ul>
-
-
-                        </div>
-                        <div className={s.profileBody}>
-                            <ul>
-                                <li>
-                                    <NavLink to={'/'}>
-                                        <FontAwesomeIcon icon={faUser}
-                                                         className={s.listAwesomeIcons}> </FontAwesomeIcon> Profile
-                                    </NavLink>
-                                </li>
-                                <li>
-                                    <NavLink to={'/'}>
-                                        <FontAwesomeIcon icon={faEnvelope}
-                                                         className={s.listAwesomeIcons}> </FontAwesomeIcon> Inbox
-                                    </NavLink>
-                                </li>
-                                <li>
-                                    <NavLink to={'/'}>
-                                        <FontAwesomeIcon icon={faFile}
-                                                         className={s.listAwesomeIcons}> </FontAwesomeIcon> Activity
-                                    </NavLink>
-                                </li>
-                            </ul>
-                            <ul>
-                                <li>
-                                    <NavLink to={'/'}>
-                                        <FontAwesomeIcon icon={faCog}
-                                                         className={s.listAwesomeIcons}> </FontAwesomeIcon> Settings
-                                    </NavLink>
-                                </li>
-                                {props.isAuth ?
-                                    <li onClick={props.logout} style={{cursor: 'pointer'}}>
-                                        <FontAwesomeIcon icon={faUnlockAlt}
-                                                         className={s.listAwesomeIcons}>
-                                        </FontAwesomeIcon> Sign Out
-                                    </li>
-                                    : <li>
-                                        <NavLink to={'/login'}>
-                                            <FontAwesomeIcon icon={faUnlockAlt}
-                                                             className={s.listAwesomeIcons}>
-                                            </FontAwesomeIcon>Sign In
-                                        </NavLink>
-                                    </li>
-                                }
-                            </ul>
-                        </div>
-                    </div>
-
-                    {/*{props.isAuth ? <div>{props.login} - <button onClick={props.logout}>Logout</button></div>*/}
-                    {/*    : <NavLink to={'/login'}>Login</NavLink>}*/}
-
-                    {/*{props.isAuth ?*/}
-                    {/*    <li onClick={props.logout}>{props.login} Logout</li>*/}
-                    {/*    : <li><NavLink to={'/login'}>Login</NavLink></li>}*/}
+                    <ProfileDropdown dropWindow={dropWindow}
+                                     setDropWindow={setDropWindow}
+                                     fullName={props.fullName}
+                                     isAuth={props.isAuth}
+                                     logout={props.logout}
+                    />
                 </div>
+
             </div>
 
         </div>
--- src/components/Header/ProfileDropdown/ProfileDropdown.jsx
+++ src/components/Header/ProfileDropdown/ProfileDropdown.jsx
@@ -1,10 +1,73 @@
 import React from 'react';
-import s from './ProfileDropdown.module.scss'
+import s from '../Header.module.scss'
 import {NavLink} from "react-router-dom";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faEnvelope, faUser} from "@fortawesome/free-regular-svg-icons";
+import {faFile} from "@fortawesome/free-regular-svg-icons/faFile";
+import {faCog, faUnlockAlt} from "@fortawesome/free-solid-svg-icons";
 
-const ProfileDropdown = (props) => {
-    return <div className={s.profileDropdown}>
+const ProfileDropdown = ({dropWindow, setDropWindow, fullName, isAuth, logout}) => {
+    return <div className={dropWindow ? s.profileDropdown : s.hideDropdown} onBlur={()=>{setDropWindow(!dropWindow)}}>
+        <div className={s.profileHead}>
+            <ul>
+                <li>
+                    <h5 className={s.fullName}>
+                        <NavLink to={'/'}>
+                            {fullName}
+                        </NavLink>
+                    </h5>
+                </li>
+                <li>
+                    <NavLink to={'/'} className={s.email}>email@email.com</NavLink>
+                </li>
+            </ul>
 
+
+        </div>
+        <div className={s.profileBody}>
+            <ul>
+                <li>
+                    <NavLink to={'/'}>
+                        <FontAwesomeIcon icon={faUser}
+                                         className={s.listAwesomeIcons}> </FontAwesomeIcon> Profile
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink to={'/'}>
+                        <FontAwesomeIcon icon={faEnvelope}
+                                         className={s.listAwesomeIcons}> </FontAwesomeIcon> Inbox
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink to={'/'}>
+                        <FontAwesomeIcon icon={faFile}
+                                         className={s.listAwesomeIcons}> </FontAwesomeIcon> Activity
+                    </NavLink>
+                </li>
+            </ul>
+            <ul>
+                <li>
+                    <NavLink to={'/'}>
+                        <FontAwesomeIcon icon={faCog}
+                                         className={s.listAwesomeIcons}> </FontAwesomeIcon> Settings
+                    </NavLink>
+                </li>
+                {isAuth ?
+                    <li onClick={logout} style={{cursor: 'pointer'}}>
+                        <FontAwesomeIcon icon={faUnlockAlt}
+                                         className={s.listAwesomeIcons}>
+                        </FontAwesomeIcon> Sign Out
+                    </li>
+                    : <li>
+                        <NavLink to={'/login'}>
+                            <FontAwesomeIcon icon={faUnlockAlt}
+                                             className={s.listAwesomeIcons}>
+                            </FontAwesomeIcon>Sign In
+                        </NavLink>
+                    </li>
+                }
+            </ul>
+        </div>
     </div>
 };
 
