48fa5da8c991f558f802c21057c357ac540a2bc2
lazychal <disposables@yandex.ru>
2020-06-30 21:54:59 +0300

    ProfileDropdownBody.jsx - done

--- src/components/Header/ProfileDropdown/ProfileDropdown.jsx
+++ src/components/Header/ProfileDropdown/ProfileDropdown.jsx
@@ -1,61 +1,14 @@
 import React from 'react';
-import s from './ProfileDropdown.module.scss'
-import {NavLink} from "react-router-dom";
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faEnvelope, faUser} from "@fortawesome/free-regular-svg-icons";
-import {faFile} from "@fortawesome/free-regular-svg-icons/faFile";
-import {faCog, faUnlockAlt} from "@fortawesome/free-solid-svg-icons";
+import s from './ProfileDropdown.module.scss';
 import ProfileDropdownHeader from "./ProfileDropdownHeader";
+import ProfileDropdownBody from "./ProfileDropdownBody";
 
 const ProfileDropdown = ({dropWindow, setDropWindow, fullName, isAuth, logout}) => {
     return <div className={dropWindow ? s.profileDropdown : s.hideDropdown} onBlur={()=>{setDropWindow(!dropWindow)}}>
 
         <ProfileDropdownHeader fullName={fullName}/>
+        <ProfileDropdownBody isAuth={isAuth} logout={logout}/>
 
-        <div className={s.profileBody}>
-            <ul>
-                <li>
-                    <NavLink to={'/'}>
-                        <FontAwesomeIcon icon={faUser}
-                                         className={s.listAwesomeIcons}> </FontAwesomeIcon> Profile
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink to={'/'}>
-                        <FontAwesomeIcon icon={faEnvelope}
-                                         className={s.listAwesomeIcons}> </FontAwesomeIcon> Inbox
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink to={'/'}>
-                        <FontAwesomeIcon icon={faFile}
-                                         className={s.listAwesomeIcons}> </FontAwesomeIcon> Activity
-                    </NavLink>
-                </li>
-            </ul>
-            <ul>
-                <li>
-                    <NavLink to={'/'}>
-                        <FontAwesomeIcon icon={faCog}
-                                         className={s.listAwesomeIcons}> </FontAwesomeIcon> Settings
-                    </NavLink>
-                </li>
-                {isAuth ?
-                    <li onClick={logout} style={{cursor: 'pointer'}}>
-                        <FontAwesomeIcon icon={faUnlockAlt}
-                                         className={s.listAwesomeIcons}>
-                        </FontAwesomeIcon> Sign Out
-                    </li>
-                    : <li>
-                        <NavLink to={'/login'}>
-                            <FontAwesomeIcon icon={faUnlockAlt}
-                                             className={s.listAwesomeIcons}>
-                            </FontAwesomeIcon>Sign In
-                        </NavLink>
-                    </li>
-                }
-            </ul>
-        </div>
     </div>
 };
 
