cedcedfcfef38ce743879b7acac206f8f38efc3e
lazychal <disposables@yandex.ru>
2020-07-04 00:07:47 +0300

    UserPhotoCard #1. Add, position

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -9,6 +9,7 @@ import Advertisement from "../AnotherWidgets/Advertisement/Advertisement";
 import FriendsZoneContainer from "../AnotherWidgets/FriendsZone/FriendsZone";
 import ProfileBackground from "./ProfileHeader/ProfileBackground";
 import ProfileMenu from "./ProfileHeader/ProfileMenu";
+import UserPhotoCard from "./Widgets/UserPhotoCard";
 
 const Profile = (props) => {
 
@@ -31,7 +32,7 @@ const Profile = (props) => {
                     <div className={s.widgetArea}>
 
                         <div className={s.subUserPhotoCardWidgetArea}>
-                            {/*<UserPhotoCard/>*/}
+                            <UserPhotoCard/>
                         </div>
                         <div className={s.subProfileInfoCardWidgetArea}>
                             {/*<ProfileInfoCard/>*/}
---
+++ src/components/Profile/Widgets/UserPhotoCard.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const backgroundStyle = {
+    backgroundColor: 'red',
+    position: 'absolute',
+    width: '100%',
+    height: '270px',
+    top: '-180px'
+};
+
+const UserPhotoCard = () => {
+
+    return <div style={backgroundStyle}>
+
+    </div>
+};
+
+export default UserPhotoCard;
