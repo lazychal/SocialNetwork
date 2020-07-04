a0f078a44c3c4b10d18b7148f90055f92cf53862
lazychal <disposables@yandex.ru>
2020-07-04 18:01:28 +0300

    UserPhotoCardContainer - style - hover - editMode for picture

--- src/components/Profile/Widgets/UserPhotoCard.jsx
+++ src/components/Profile/Widgets/UserPhotoCard.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import s from './UserPhotoCard.module.scss';
 import {useSelector} from "react-redux";
+import {faPen} from "@fortawesome/free-solid-svg-icons";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 
 const UserPhotoCard = ({userPic}) => {
 
     return <div className={s.userPhotoCard}>
         <img src={userPic} alt="User"/>
+        <div className={s.editIconContainer}>
+                <FontAwesomeIcon icon={faPen} className={s.faPen}></FontAwesomeIcon>
+        </div>
     </div>
 };
 export const UserPhotoCardContainer = () => {
